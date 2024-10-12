93bf45b9a99c118509b9947f1b3d56d26be3f254
Thach.Le <thachln@gmail.com>
2024-10-12 22:52:48 +0700

    Try to run YOLO11 to detect objects from an image.

---
+++ test/run_yolo11.py
@@ -0,0 +1,19 @@
+# Refer: https://docs.ultralytics.com/models/yolo11/#__tabbed_2_1
+import math
+from ultralytics import YOLO
+
+# Load a COCO-pretrained YOLO11x model
+model = YOLO("yolo11n.pt")
+
+# Run inference with the YOLO11n model on the 'bus.jpg' image
+image_frame = r"\Keyframe\Keyframes_L01\keyframes\L01_V001\032.jpg"
+results = model(image_frame)
+
+for r in results:
+    for box in r.boxes:
+        confidence = math.ceil((box.conf[0] * 100)) / 100
+        # x1, y1, x2, y2 = box.xyxy[0]
+
+        idxClassName = int(box.cls[0])
+        className = r.names[idxClassName]
+        print(className, ' ', confidence)
